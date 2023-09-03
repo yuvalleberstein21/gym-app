be69a74250c8e34e6b9289e52f3db5bb877e1acd
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-09-03 11:44:41 +0300

    final

--- gym-typescript/src/scenes/footer/index.tsx
+++ gym-typescript/src/scenes/footer/index.tsx
@@ -26,7 +26,7 @@ function Footer({}: Props) {
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
           <p className="my-5">Tempus meus mattis risus volutpat egats.</p>
-          <p>(333)425-6825</p>
+          <p>(+972)50-952-5201</p>
         </div>
       </div>
     </footer>
