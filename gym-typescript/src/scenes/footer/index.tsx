2b9464f8981008be48901845eabfe3dd24b3d086
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-09-03 11:43:17 +0300

    final app

---
+++ gym-typescript/src/scenes/footer/index.tsx
@@ -0,0 +1,36 @@
+import Logo from "@/assets/Logo.png";
+
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img src={Logo} alt="logo" />
+          <p className="my-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nemo
+            debitis asperiores voluptates, temporibus explicabo? Obcaecati
+            molestiae repudiandae excepturi incidunt ea.
+          </p>
+          <p>© Evogym All Rights Reserved.</p>
+        </div>
+
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senceturs</p>
+          <p className="my-5">Et gravida id et etiam</p>
+          <p>Ullamcorper vivamus</p>
+        </div>
+
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Tempus meus mattis risus volutpat egats.</p>
+          <p>(333)425-6825</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
